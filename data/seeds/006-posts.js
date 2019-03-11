e99b744d70973dfbcf8ec549a8d34cd5c98da280
Moonkaman <nickbatmanglidj@gmail.com>
2019-03-11 11:12:56 -0600

    Create posts migrations and seeds

---
+++ data/seeds/006-posts.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('posts').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('posts').insert([
+        {user_id: 1, post_content: 'Spaghetti and meatballs', likes: 0},
+        {user_id: 2, post_content: 'Dogs are great', likes: 0},
+        {user_id: 3, post_content: 'Anyone ever hear of WoW?', likes: 0}
+      ]);
+    });
+};
