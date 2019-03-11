731b0233b6af54e84e8fae93b0ae8e9c7546d225
Moonkaman <nickbatmanglidj@gmail.com>
2019-03-11 11:46:56 -0600

    Create login endpoint and check for correct password

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const configMiddleware = require('./middleware');
+const authRouter = require('../auth/authRouter');
 
 const server = express();
 
 configMiddleware(server);
+server.use('/auth', authRouter);
 
 server.get('/', (req, res) => res.status(200).send('Working so far'));
 
--- auth/authRouter.js
+++ auth/authRouter.js
@@ -1,5 +1,22 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 
+const db = require('../data/helpers/authHelpers');
 
+router.post('/login', (req, res) => {
+  if(req.body.username && req.body.password) {
+    db.findBy({username: req.body.username})
+      .then(user => {
+        if(user && bcrypt.compareSync(req.body.password, user.password)) {
+          res.status(200).json({message: `Successfully logged in, welcome ${user.name}!`})
+        } else {
+          res.status(401).json({message: 'Invalid credentials'});
+        }
+      })
+      .catch(err => res.status(500).json({message: 'Unable to login at this time', err}));
+  } else {
+    res.status(400).json({message: 'Please provide a username and password'});
+  }
+})
 
 module.exports = router;
