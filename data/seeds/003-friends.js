ceabeccd56d0f9f108dee2eda9ac7113bc41447b
Moonkaman <nickbatmanglidj@gmail.com>
2019-03-11 14:41:47 -0600

    change truncates to deletes in seeds

--- data/seeds/003-friends.js
+++ data/seeds/003-friends.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('friends').truncate()
+  return knex('friends').del()
     .then(function () {
       // Inserts seed entries
       return knex('friends').insert([
--- data/seeds/004-user_bubbles.js
+++ data/seeds/004-user_bubbles.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('user_bubbles').truncate()
+  return knex('user_bubbles').del()
     .then(function () {
       // Inserts seed entries
       return knex('user_bubbles').insert([
