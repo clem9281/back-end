48d5a954e3dd14e572051a8aeadeb9bd30ff1761
Moonkaman <nickbatmanglidj@gmail.com>
2019-03-11 15:09:07 -0600

    More heroku seed fixes part 2

--- data/seeds/003-friends.js
+++ data/seeds/003-friends.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('friends').insert([
-        {user_id: 1, friend_id: 2},
-        {user_id: 1, friend_id: 3},
-        {user_id: 2, friend_id: 3}
+        {id: 1, user_id: 1, friend_id: 2},
+        {id: 2, user_id: 1, friend_id: 3},
+        {id: 3, user_id: 2, friend_id: 3}
       ]);
     });
 };
--- data/seeds/005-user_bubbles.js
+++ data/seeds/005-user_bubbles.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('user_bubbles').insert([
-        {student_id: 1, bubble_id: 1},
-        {student_id: 2, bubble_id: 1},
-        {student_id: 2, bubble_id: 3}
+        {id: 1, student_id: 1, bubble_id: 1},
+        {id: 2, student_id: 2, bubble_id: 1},
+        {id: 3, student_id: 2, bubble_id: 3}
       ]);
     });
 };
