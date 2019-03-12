0a240e048250aab9fdbd7a47209030e0efd3ef41
Moonkaman <nickbatmanglidj@gmail.com>
2019-03-12 15:05:50 -0600

    More heroku fixes

--- data/seeds/002-user_profiles.js
+++ data/seeds/002-user_profiles.js
@@ -3,8 +3,8 @@ const bcrypt = require('bcryptjs');
 exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('user_profiles').insert([
-        {school_id: 1, name: 'Nick Batmanglidj', username: 'Nbatman', bio: 'It me.', password: bcrypt.hashSync('pass', 12)},
-        {school_id: 1, name: 'Clint Kunz', username: 'Ckunz', bio: 'It also me guys.', password: bcrypt.hashSync('pass', 12)},
-        {school_id: 2, name: 'Harry Potter', username: 'Hpotter', bio: 'Where are we ron?', password: bcrypt.hashSync('pass', 12)}
+        {id: 1, school_id: 1, name: 'Nick Batmanglidj', username: 'Nbatman', bio: 'It me.', password: bcrypt.hashSync('pass', 12)},
+        {id: 2, school_id: 1, name: 'Clint Kunz', username: 'Ckunz', bio: 'It also me guys.', password: bcrypt.hashSync('pass', 12)},
+        {id: 3, school_id: 2, name: 'Harry Potter', username: 'Hpotter', bio: 'Where are we ron?', password: bcrypt.hashSync('pass', 12)}
       ]);
 };
