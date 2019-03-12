a0a6f8874679d1e47b80be76ba860ac5feeeec1a
Moonkaman <nickbatmanglidj@gmail.com>
2019-03-12 15:41:29 -0600

    Heroku test fix

--- data/seeds/009-schools_bubbles.js
+++ data/seeds/009-schools_bubbles.js
@@ -2,11 +2,11 @@
 exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('schools_bubbles').insert([
-        {school_id: 1, bubble_id: 1},
-        {school_id: 1, bubble_id: 2},
-        {school_id: 1, bubble_id: 3},
-        {school_id: 2, bubble_id: 4},
-        {school_id: 2, bubble_id: 5},
-        {school_id: 2, bubble_id: 6},
+        {id: 1, school_id: 1, bubble_id: 1},
+        {id: 2, school_id: 1, bubble_id: 2},
+        {id: 3, school_id: 1, bubble_id: 3},
+        {id: 4, school_id: 2, bubble_id: 4},
+        {id: 5, school_id: 2, bubble_id: 5},
+        {id: 6, school_id: 2, bubble_id: 6},
       ]);
 };
