84896485a7d3a793329651cf08f05ad54356583d
Moonkaman <nickbatmanglidj@gmail.com>
2019-03-13 16:34:20 -0600

    Delete endpoint fix

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -24,22 +24,15 @@ router.get('/filter/:id', (req, res) => {
 })
 
 router.delete('/:id', (req, res) => {
-  db.get(req.params.id)
-    .then( post => {
-      if(String(post.user_id) === req.params.id) {
-        db.deletePost(req.params.id)
-          .then(count => {
-            if(count > 0) {
-              res.status(204).end();
-            } else {
-              res.status(404).json({message: 'The post you tried to delete was not found'});
-            }
-          })
-          .catch(err => res.status(500).json({message: 'Post could not be deleted at this time', err}));
+  db.deletePost(req.params.id)
+    .then(count => {
+      if(count > 0) {
+        res.status(204).end();
       } else {
-        res.status(401).json({message: 'The post you tried to delete does not belong to you'});
+        res.status(404).json({message: 'The post you tried to delete was not found'});
       }
     })
+    .catch(err => res.status(500).json({message: 'Post could not be deleted at this time', err}));
 })
 
 router.put('/:id', (req, res) => {
