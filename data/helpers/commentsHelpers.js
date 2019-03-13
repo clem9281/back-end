1e4b8f6d8de5563f2100d9dc99cd2f0522a5e751
Moonkaman <nickbatmanglidj@gmail.com>
2019-03-13 14:22:32 -0600

    heroku fix

--- data/helpers/commentsHelpers.js
+++ data/helpers/commentsHelpers.js
@@ -5,7 +5,7 @@ module.exports = {
 }
 
 function addComment(comment) {
-  return db('post_comments').insert(comment)
+  return db('post_comments').returning('id').insert(comment)
     .then(id => {
       return db.select('pc.id', 'pc.user_id', 'u.name', 'pc.comment', 'pc.created_at').from('post_comments as pc')
       .innerJoin('user_profiles as u', 'pc.user_id', 'u.id').where({'pc.id': id[0]});
