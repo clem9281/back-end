5c9261089e408e2bb8efde6be2ad01cc5d75bca7
Moonkaman <nickbatmanglidj@gmail.com>
2019-03-13 14:46:35 -0600

    Create knex testing config, add cross-env & create test script

--- knexfile.js
+++ knexfile.js
@@ -21,6 +21,19 @@ module.exports = {
       directory: './data/seeds'
     }
   },
+  testing: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/testingDB.sqlite3'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL || prodDbConnection,
