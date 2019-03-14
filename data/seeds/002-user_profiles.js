99896fc51b5b21ba2d7eae043a1d7f8de78bbe88
Moonkaman <nickbatmanglidj@gmail.com>
2019-03-14 16:51:15 -0600

    Seed fix

--- data/seeds/002-user_profiles.js
+++ data/seeds/002-user_profiles.js
@@ -7,7 +7,7 @@ exports.seed = async (knex, Promise) => {
         {school_id: 1, name: 'Clint Kunz', username: 'Ckunz', bio: 'It also me guys.', password: bcrypt.hashSync('pass', 12)},
         {school_id: 2, name: 'Harry Potter', username: 'Hpotter', bio: 'Where are we ron?', password: bcrypt.hashSync('pass', 12)},
         {school_id: 2, name: 'Hermione Granger', username: 'Hgranger', bio: 'Read books or something', password: bcrypt.hashSync('pass', 12)},
-        {school_id: 2, name: 'Ron Weasley', username: 'Rweezy', bio: 'I don\'t know harry?', password: bcrypt.hashSync('pass', 12)},
+        {school_id: 2, name: 'Ron Weasley', username: 'Rweezy', bio: 'I am ron weasley, what else should I put in here harry', password: bcrypt.hashSync('pass', 12)},
         {school_id: 3, name: 'Spongebob Squarepants', username: 'Sbob', bio: 'I love working at the krusty krab', password: bcrypt.hashSync('pass', 12)},
         {school_id: 3, name: 'Patrick Star', username: 'Pstar', bio: 'Has anyone seen spongebob?', password: bcrypt.hashSync('pass', 12)},
         {school_id: 3, name: 'My leg guy', username: 'Mleg', bio: 'MY LEG', password: bcrypt.hashSync('pass', 12)},
